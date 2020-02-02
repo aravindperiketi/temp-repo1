69bd419f65f9c7363100121553bd37f6ad4aa1e4
aravind <aravindperiketi@gmail.com>
2020-02-02 14:20:25 +0530

    sucuss fully changed

--- 1.c
+++ 1.c
@@ -3,14 +3,16 @@
 
 int func(int *x,int *y)
 {
-	*x=*x+*y;
+	++(*x);
+	++(*y);
 }
 
 int main()
 {
 int a=10,n=4;
 int b=20,r=7;
-printf("myadd:%d",myadd(r,n));
+printf("myadd:%d\n",myadd(r,n));
 
-printf("%d\n",func(&a,&b));
+func(&a,&b);
+printf("%d\n%d\n",a,b);
 }
