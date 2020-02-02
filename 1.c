48b76bbcf49a6330550225a1a55692148d45ead8
aravind <aravindperiketi@gmail.com>
2020-02-02 13:56:48 +0530

    added adition program

--- 1.c
+++ 1.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
+#include"mymath.h"
 
 int func(int *x,int *y)
 {
 	*x=*x+*y;
 }
+
 int main()
 {
 int a=10;
 int b=20;
+printf("myadd:%d",myadd(x,y));
+
 printf("%d\n",func(&a,&b));
 }
